f3cb76afe6ff906ae9660d3845b7dcfba7d7cc2c
Sammy Sahnine <sammysahnine0@gmail.com>
2024-08-21 01:53:41 +0100

    First time using SwiftUI, getting started with layout.

--- DisCompress/ContentView.swift
+++ DisCompress/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isDragging = false
+    @State private var draggedItem: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+            Text("DisCompress")
+                .foregroundStyle(.foreground)
+                .padding()
+                .fontWidth(.expanded)
+                .font(.system(size: 36))
+            
+            RoundedRectangle(cornerRadius: 30)
+                .fill(isDragging ? Color.gray.opacity(0.5) : Color.gray.opacity(0.2))
+                .frame(minWidth: 300, maxWidth: 300, minHeight: 300, maxHeight: 300)
+                .padding(.trailing, 10)
+                .padding(.leading, 10)
+                .padding(.bottom, 30)
+                .overlay(
+                    Text(draggedItem ?? "Drop file here")
+                        .foregroundStyle(.foreground)
+                        .fontWeight(.bold)
+                        .fontDesign(.monospaced)
+                        
+                )
+                .onDrop(of: ["public.text"], isTargeted: $isDragging) { providers, location in
+                    return handle(providers: providers)
+                }
         }
-        .padding()
+    }
+    
+    private func handle(providers: [NSItemProvider]) -> Bool {
+        return false
+    }
+}
+
+struct DragAndDropBox_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
