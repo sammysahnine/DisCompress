1ad4ec2a6ee327e4cd7ef9bf9436c19588d3b2d9
Sammy Sahnine <sammysahnine0@gmail.com>
2024-08-21 01:27:01 +0100

    Initial Commit

---
+++ DisCompress/DisCompressApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisCompressApp.swift
+//  DisCompress
+//
+//  Created by Sammy Sahnine on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DisCompressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
